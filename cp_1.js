4e395943869dcbcdb5a66065f58a060dc2c8ff63
Nikita Siniayev <ns21@usf.edu>
2025-10-17 15:18:37 -0400

    const add

---
+++ cp_1.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("feedbackForm");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const commentsInput = document.getElementById("comments");
+const charCount = document.getElementById("charCount");
+const feedbackDisplay =
+  document.getElementById("feedback-display") ||
+  document.getElementById("feedbackList");
+
