6c2373257f10566d4cebd58362c88f1500c23e97
Nikita Siniayev <ns21@usf.edu>
2025-10-16 21:19:58 -0400

    css changed

--- cp_1.js
+++
@@ -1,8 +0,0 @@
-const commentsInput = document.getElementById("comments");
-const charCount = document.getElementById("charCount");
-
-// update count on input
-commentsInput.addEventListener("input", () => {
-  const length = commentsInput.value.length;
-  charCount.textContent = `${length} characters`;
-});
